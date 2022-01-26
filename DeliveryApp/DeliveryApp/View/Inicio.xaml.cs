3a6bd7f4bd44a58d0fbe144ae61c14c266d70f26
iagoa <iagoantunes.f@gmail.com>
2022-01-26 16:38:15 -0300

    Adicionar arquivos de projeto.

---
+++ DeliveryApp/DeliveryApp/View/Inicio.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DeliveryApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+        private void GoHome(object sender,EventArgs args)
+        {
+            App.Current.MainPage = new View.Home();
+        }
+    }
+}
